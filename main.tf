4b5d4b124d3ecf3e86f0cf3c98bf85d437d83d69
Randeer Lalanga <39120946+randeer@users.noreply.github.com>
2024-08-24 15:54:06 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+resource "tls_private_key" "private-key" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "rashmika" {
+  filename = "rashmika.txt"
+  content = tls_private_key.private-key.private_key_pem
+}
+
+resource "tls_private_key" "private-key2" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "dilshani" {
+  filename = "dilshani.txt"
+  content = "Dilshani Lakshika Dias"
+}
+
+variable "girl_name" {
+  default = "Dilshani Dias"
+  type = string
+}
+
+variable "email_id" {
+  description = "This is user's sensitive data"
+  sensitive = true
+}
+
+variable "my_company" {
+  default = "Halexo"
+  type = string
+}
+
+output "my_girl" {
+  value = "My crush is ${var.girl_name} and my company is ${var.my_company}"
+}
+
+output "email_ID" {
+  value = var.email_id
+  sensitive = true
+}
